f4ab864b3c4595b6138e74e398c817f8e84a20ea
manuti <manuti@gmail.com>
2019-04-05 17:21:28 +0200

    Update lote-small-pdf.py

--- lote-small-pdf.py
+++ lote-small-pdf.py
@@ -14,7 +14,7 @@ while files or workers:
         f = files[0]
         files = files[1:]
 # aquí ocurre la magia
-        w = subprocess.Popen(['ps2pdf', f,
+        w = subprocess.Popen(['ps2pdf -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -dMaxSubsetPct=100 -dAutoFilterColorImages=false -dColorImageFilter=/FlateEncode -dAutoFilterGrayImages=false -dGrayImageFilter=/FlateEncode -dAutoFilterMonoImages=false -dMonoImageFilter=/CCITTFaxEncode', f,
             os.path.splitext(f)[0]+'_XS.pdf'])
 # aquí ocurre la magia
         workers.append(w)
