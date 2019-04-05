8363c758f53d6702e339f090dfa8c94c05c07f31
manuti <manuti@gmail.com>
2019-04-05 17:55:56 +0200

    funcionando - working
    algunas veces los PDF no se convierten bien y otras son más grandes que los originales
    sometimes some PDF are improperly converted or are bigger than the original

--- lote-small-pdf.py
+++ lote-small-pdf.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
 # untested
-# está sin probar
+# sin probar
 # ghostscript must be installed -> sudo apt-get install ghostscript
 # ghostscript debe estar instalado -> sudo apt-get install ghostscript
 
 import os, time, glob, subprocess
 
-files = glob.glob('*.png')
+files = glob.glob('*.pdf')
 
 workers = []
 while files or workers:
     while len(workers) < 4 and files:
         f = files[0]
         files = files[1:]
-# aquí ocurre la magia
-        w = subprocess.Popen(['ps2pdf -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -dMaxSubsetPct=100 -dAutoFilterColorImages=false -dColorImageFilter=/FlateEncode -dAutoFilterGrayImages=false -dGrayImageFilter=/FlateEncode -dAutoFilterMonoImages=false -dMonoImageFilter=/CCITTFaxEncode', f,
-            os.path.splitext(f)[0]+'_XS.pdf'])
-# aquí ocurre la magia
+# aqui ocurre la magia
+        w = subprocess.Popen(['ps2pdf', '-dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -dMaxSubsetPct=100 -dAutoFilterColorImages=false -dColorImageFilter=/FlateEncode -dAutoFilterGrayImages=false -dGrayImageFilter=/FlateEncode -dAutoFilterMonoImages=false -dMonoImageFilter=/CCITTFaxEncode', f])
+# aqui ocurre la magia
         workers.append(w)
     for w in list(workers):
         if w.poll() is not None:
